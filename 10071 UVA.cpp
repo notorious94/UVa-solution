fe24d889423cd2b49c51467d247a556dd7c533cb
MD. Istiad Hossain <akib94dhk@gmail.com>
2017-06-25 12:53:22 +0600

    Create 10071 UVA.cpp

---
+++ 10071 UVA.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+    int v, t;
+
+    while(scanf("%d%d", &v,&t)!=EOF)
+        printf("%d\n", 2*v*t);
+
+    return 0;
+}
