e0d93434a6cf30ad309a4bbbf5b96d5e54ccd64e
MD. Istiad Hossain <akib94dhk@gmail.com>
2017-07-27 23:26:23 +0600

    Create 1124 UVA.cpp

---
+++ 1124 UVA.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/// M A C R O Starts Here
+#define pf printf
+#define sf scanf
+#define MAX 500000
+#define sif(a) scanf("%d",&a)
+#define pif(a) printf("%d\n",a)
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+int main()
+{
+    //freopen("in.txt","r", stdin);
+    //freopen("out.txt","w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    char s[MAX];
+
+    while(gets(s))
+        pf("%s\n",s);
+
+    return 0;
+}
