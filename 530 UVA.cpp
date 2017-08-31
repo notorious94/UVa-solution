c6ae52e89d218694a8a3d86fb00f6fed663ec10a
MD. Istiad Hossain <akib94dhk@gmail.com>
2017-09-01 01:17:19 +0600

    Create 530 UVA.cpp

---
+++ 530 UVA.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/// M A C R O Starts Here
+#define pf printf
+#define sf scanf
+#define MAX 500000
+#define pi acos(-1.0)
+#define get_stl(s) getline(cin,s)
+#define sif(a) scanf("%d",&a)
+#define pif(a) printf("%d\n",a)
+#define puf(a) printf("%llu\n",a)
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+ull binomialCoeff(ull n, ull k)
+{
+    ull res = 1;
+
+    if (k > n - k)
+        k = n - k;
+
+    for (int i = 0; i < k; ++i)
+    {
+        res *= (n - i);
+        res /= (i + 1);
+    }
+
+    return res;
+}
+
+
+int main()
+{
+    //freopen("in.txt","r", stdin);
+    //freopen("out.txt","w", stdout);
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ull n,k;
+    while(sf("%llu%llu", &n, &k))
+    {
+        if(!n&&!k)
+            break;
+        puf(binomialCoeff(n,k));
+    }
+
+    return 0;
+}
