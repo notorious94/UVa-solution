201a4ccfdd6f68a313c603c6c715826d21788cac
MD. Istiad Hossain <akib94dhk@gmail.com>
2017-09-28 22:56:34 +0600

    Create 12149 UVA.cpp

---
+++ 12149 UVA.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/// M A C R O Starts Here
+#define pf printf
+#define sf scanf
+#define MAX 500000
+#define INF 99999
+#define pi acos(-1.0)
+#define get_stl(s) getline(cin,s)
+#define sif(a) scanf("%d",&a)
+#define pif(a) printf("%d\n",a)
+#define puf(a) printf("%llu\n",a)
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int dx[]={0,0,1,-1,-1,1,-1,1};
+int dy[]={-1,1,0,0,1,1,-1,-1};
+int dz[]={0,0,+1,-1,-1,+1,-1,+1};
+
+int main()
+{
+    //freopen("in.txt","r", stdin);
+    //freopen("out.txt","w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int arr[101],n=0;
+
+    for(int i=1;i<=100;i++)
+    {
+        n += i*i;
+        arr[i]=n;
+    }
+
+    while(sif(n)!=EOF)
+    {
+        if(!n)
+            break;
+        pif(arr[n]);
+    }
+
+    return 0;
+}
