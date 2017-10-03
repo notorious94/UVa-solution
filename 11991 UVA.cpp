13efae90554b2e6330fa26ad3ac62c13acfd11ce
MD. Istiad Hossain <akib94dhk@gmail.com>
2017-10-03 14:57:47 +0600

    Update 11991 UVA.cpp

--- 11991 UVA.cpp
+++ 11991 UVA.cpp
@@ -3,26 +3,35 @@ using namespace std;
 
 int main()
 {
-    //freopen("in.txt", "r", stdin);
-    //freopen("out.txt", "w", stdout);
+    //freopen("in.txt","r", stdin);
+    //freopen("out.txt","w", stdout);
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int n,m,k,v;
-    vector<vector<int> > data;
-    while(scanf("%d%d",&n,&m)!=EOF)
+
+    map<int, vector<int> > index;
+
+    while(scanf("%d%d", &n, &m)!=EOF)
     {
-        data.assign(1000000,vector<int>());
         for(int i=1;i<=n;i++)
         {
-            scanf("%d",&v);
-            data[v].push_back(i);
+            scanf("%d",&k);
+            index[k].push_back(i);
         }
-        for(int i=1;i<=m;i++)
+
+        while(m--)
         {
-            scanf("%d%d",&k,&v);
-            if(data[v].size()>=k)
-                printf("%d\n",data[v][k-1]);
-            else
+            scanf("%d%d",&k, &v);
+            if(k>index[v].size())
                 printf("0\n");
+            else
+                printf("%d\n",index[v][k-1]);
         }
+
+        index.clear();
     }
+
     return 0;
 }
