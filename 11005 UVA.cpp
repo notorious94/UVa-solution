d0d8287d68b9e0834e82636591fded8d8cc04585
MD. Istiad Hossain <akib94dhk@gmail.com>
2017-10-09 21:59:55 +0600

    Update 11005 UVA.cpp

--- 11005 UVA.cpp
+++ 11005 UVA.cpp
@@ -16,86 +16,89 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
-string digit = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"; /// Holding Digits
-map<char, int>cost; /// Holding Print Cost
+string digit = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+map<char, int>cost;
 
-int convert_in_all_base(int decimal, int base);
+int convert(int dec, int base)
+{
+    if(!dec)
+        return cost[digit[0]];
+    int money=0;
+
+    while(dec)
+    {
+        int m = dec%base;
+        dec/=base;
+        money += cost[digit[m]];
+    }
+
+    return money;
+}
 
 int main()
 {
     //freopen("in.txt","r", stdin);
     //freopen("out.txt","w", stdout);
 
-    int t, q, c, n, print_cost;
-    map<int, int>ans;
-    vector<int>Base;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    scanf("%d", &t);/// No. of Test Cases
+    int t,q,n,c;
+    vector<int>result;
 
-    for(int kase = 1; kase<=t; kase++)
+    scanf("%d", &t);
+
+    for(int kase = 1; kase<=t;kase++)
     {
-        printf("Case %d:\n", kase);/// Case Print
+        printf("Case %d:\n",kase);
 
-        for(int i=0;digit[i];i++) /// Adding_Cost
+        for(int i=0;digit[i];i++)
         {
-            scanf("%d", &c);
+            sif(c);
             cost[digit[i]]=c;
         }
 
-        scanf("%d", &q); /// Query
+        sif(q);
 
         while(q--)
         {
-            scanf("%d",&n); /// Decimal Value
+            sif(n);
+            int money, min = INT_MAX;
 
-            ans.clear();
-            Base.clear();
 
-            printf("Cheapest base(s) for number %d: ", n);
+            printf("Cheapest base(s) for number %d: ",n);
 
-            int min_cost = INT_MAX;
-
-            for(int base = 2; base <= 36; base++)
+            for(int base =2 ;base<=36;base++)
             {
-                print_cost = convert_in_all_base(n,base); /// Getting Cost for every base
+                money = convert(n,base);
 
-                ans[base] = print_cost; /// Storing Cost
+                if(min>money)
+                {
+                    result.clear();
+                    result.push_back(base);
+                    min = money;
+                }
 
-                if(min_cost>print_cost) /// Finding the minimum cost value
-                    min_cost=print_cost;
-
+                else if(min==money)
+                    result.push_back(base);
             }
 
-            for(int i=2;i<=36;i++) /// Fonding Base(s) with minimum cost
-                if(ans[i]==min_cost)
-                    Base.push_back(i);
+            for(int i=0;;i++)
+            {
 
-            for(int i=0;i<Base.size()-1;i++) /// Printing Answer
-                printf("%d ",Base[i]);
+                if(i==result.size()-1)
+                {
+                    printf("%d\n",result[i]);
+                    result.clear();
+                    break;
+                }
+                printf("%d ",result[i]);
+            }
 
-            printf("%d\n",Base[Base.size()-1]); /// Lase Answer
         }
-
         if(kase!=t)
             printf("\n");
-
     }
+
     return 0;
-}
-
-int convert_in_all_base(int decimal, int base)
-{
-    if(!decimal)
-        return cost[digit[0]];
-
-    int Cost = 0;
-
-    while(decimal)
-    {
-        int m = decimal%base;
-        decimal/=base;
-        Cost += cost[digit[m]];
-    }
-
-    return Cost;
 }
