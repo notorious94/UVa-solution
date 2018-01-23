0b87b723d700a70f50c31763bdd54da950e0e8ee
MD. Istiad Hossain <akib94dhk@gmail.com>
2018-01-23 08:42:09 +0600

    Create 10684 UVA.cpp

---
+++ 10684 UVA.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/// M A C R O Starts Here
+#define pf printf
+#define sf scanf
+#define MAX 500000
+#define MOD 100000007
+#define INF INT_MAX
+#define pi acos(-1.0)
+#define get_stl(s) getline(cin,s)
+#define sif(a) scanf("%d",&a)
+#define pif(a) printf("%d\n",a)
+#define puf(a) printf("%llu\n",a)
+#define pii pair<int, int>
+#define mem(name, value) memset(name, value, sizeof(name))
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int dp[10001];
+
+int main()
+{
+    //freopen("in.txt","r", stdin);
+    //freopen("out.txt","w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    int cost[10001];
+
+    while(scanf("%d",&n)==1)
+    {
+        if(!n)  break;
+
+        for(int i=1;i<=n;i++)
+            scanf("%d",&cost[i]);
+        dp[0] = 0;
+        int ans = 0;
+        for(int i=1;i<=n;i++)
+            ans = max (ans, dp[i] = max(dp[i-1]+cost[i],cost[i]));
+
+        if(ans<=0)
+            pf("Losing streak.\n");
+        else
+            pf("The maximum winning streak is %d.\n",ans);
+    }
+    return 0;
+}
