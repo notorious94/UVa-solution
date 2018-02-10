b1e602c12c44741cfa4f68e1f0fae831fe5cfbb7
MD. Istiad Hossain <akib94dhk@gmail.com>
2018-02-10 22:48:43 +0600

    Create 11547 UVA.cpp

---
+++ 11547 UVA.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/// M A C R O Starts Here
+#define pf printf
+#define sf scanf
+#define MAX 500000
+#define MOD 100000007
+#define INF INT_MAX
+#define pi acos(-1.0)
+#define get_stl(s) getline(cin,s)
+#define sif(a) scanf("%d",&a)
+#define pif(a) printf("%d\n",a)
+#define puf(a) printf("%llu\n",a)
+#define pii pair<int, int>
+#define mem(name, value) memset(name, value, sizeof(name))
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main()
+{
+    //freopen("in.txt","r", stdin);
+    //freopen("out.txt","w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t,n,r;
+    sif(t);
+    while(t--)
+    {
+        sif(n);
+        r = ((((((((n*567)/9)+7492)*235)/47)-498)/10))%10;
+        pif(abs(r));
+    }
+    return 0;
+}
