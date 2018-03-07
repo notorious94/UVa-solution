d99b5f537045d26319442e6e3425bfda573d1941
MD. Istiad Hossain <akib94dhk@gmail.com>
2018-03-07 22:42:09 +0600

    Create 10098 UVA.cpp

---
+++ 10098 UVA.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/// M A C R O Starts Here
+#define pf printf
+#define sf scanf
+#define MAX 500000
+#define MOD 100000007
+#define INF INT_MAX
+#define pi acos(-1.0)
+#define get_stl(s) getline(cin,s)
+#define sif(a) scanf("%d",&a)
+#define pif(a) printf("%d\n",a)
+#define puf(a) printf("%llu\n",a)
+#define pii pair<int, int>
+#define mem(name, value) memset(name, value, sizeof(name))
+#define all(name) name.begin(),name.end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int dx[]={0,0,1,-1,-1,1,-1,1};
+int dy[]={-1,1,0,0,1,1,-1,-1};
+int dz[]={0,0,+1,-1,-1,+1,-1,+1};
+
+int isLeft(double a, double b, double c, double d, double e, double f)
+{
+    double r = 0.5*((a*(d-f))+(b*(e-c))+((c*f)-(d*e)));
+    if(r==0.00) return 0;
+    if(r<0)     return -1;
+    return 1;
+}
+
+int main()
+{
+    //freopen("in.txt","r", stdin);
+    //freopen("out.txt","w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    char s[15];
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%s",&s);
+        string a = s;
+        sort(all(a));
+        printf("%s\n",a.c_str());
+        while(next_permutation(all(a)))
+            printf("%s\n",a.c_str());
+        if(t)   printf("\n");
+    }
+    puts("");
+    return 0;
+}
