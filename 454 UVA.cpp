f23909d0a96bfff2ee6fb6307690c42d0c59127e
MD. Istiad Hossain <akib94dhk@gmail.com>
2018-03-08 00:48:47 +0600

    Update 454 UVA.cpp

--- 454 UVA.cpp
+++ 454 UVA.cpp
@@ -71,15 +71,5 @@ int main()
         if(--t)
             puts("");
     }
-    /*char str[] ="This a sample string";
-    char * pch;
-    //printf ("Splitting string \"%s\" into tokens:\n",str);
-    pch = strtok (str," ");
-    while (pch)
-    {
-        printf ("%s\n",pch);
-        pch = strtok (NULL, " ,.-");
-    }*/
-
     return 0;
 }
