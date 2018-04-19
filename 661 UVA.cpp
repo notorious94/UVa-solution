910a62fe9261c93b88433a2430c90b08e0bd56bf
MD. Istiad Hossain <akib94dhk@gmail.com>
2018-04-19 23:06:43 +0600

    Create 661 UVA.cpp

---
+++ 661 UVA.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/// M A C R O Starts Here
+#define pf printf
+#define sf scanf
+#define MAX 500000
+#define MOD 100000007
+#define INF INT_MAX
+#define pi acos(-1.0)
+#define get_stl(s) getline(cin,s)
+#define sif(a) scanf("%d",&a)
+#define pif(a) printf("%d\n",a)
+#define puf(a) printf("%llu\n",a)
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define mem(name, value) memset(name, value, sizeof(name))
+#define all(name) name.begin(),name.end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int dx[]={0,0,1,-1,-1,1,-1,1};
+int dy[]={-1,1,0,0,1,1,-1,-1};
+int dz[]={0,0,1,-1,-1,1,-1,1};
+
+int isLeft(double a, double b, double c, double d, double e, double f)
+{
+    double r = 0.5*((a*(d-f))+(b*(e-c))+((c*f)-(d*e)));
+    if(r==0.00) return 0;
+    if(r<0)     return -1;
+    return 1;
+}
+
+double _distance(double x1,double y1,double x2,double y2)
+{
+    double x1x2 = x1 - x2;
+    double y1y2 = y1 - y2;
+    return sqrt((x1x2*x1x2)+(y1y2*y1y2));
+}
+
+int main()
+{
+    //freopen("in.txt","r", stdin);
+    //freopen("out.txt","w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int use[21],amps[21];
+    int kase = 0;
+    int n,m,c;
+    while(scanf("%d%d%d",&n,&m,&c))
+    {
+        if(!n&&!m&&!c)  break;
+
+        for(int i=1;i<=n;i++)
+        {
+            scanf("%d",&amps[i]);
+            use[i]=0;
+        }
+
+        int max_use = 0,id,power = 0;
+        bool fuse = false;
+
+        while(m--)
+        {
+            scanf("%d",&id);
+
+            use[id]++;
+
+            if(use[id]&1)
+            {
+                power+=amps[id];
+                max_use=max(max_use,power);
+            }
+
+            else
+            {
+                power-=amps[id];
+                max_use=max(max_use,power);
+            }
+
+            if(power>c) fuse = true;
+        }
+
+        printf("Sequence %d\n",++kase);
+
+        if(fuse)
+            puts("Fuse was blown.\n");
+
+        else
+        {
+            printf("Fuse was not blown.\n");
+            printf("Maximal power consumption was %d amperes.\n\n",max_use);
+        }
+
+    }
+    return 0;
+}
