15fa8afb5780724c96c191f8ca6fd8be0d7360db
MD. Istiad Hossain <akib94dhk@gmail.com>
2018-05-02 11:17:18 +0600

    Update 10226 UVA.cpp

--- 10226 UVA.cpp
+++ 10226 UVA.cpp
@@ -5,59 +5,97 @@ using namespace std;
 #define pf printf
 #define sf scanf
 #define MAX 500000
-#define INF 0x7FFFFFFF
+#define MOD 100000007
+#define INF INT_MAX
 #define pi acos(-1.0)
 #define get_stl(s) getline(cin,s)
 #define sif(a) scanf("%d",&a)
 #define pif(a) printf("%d\n",a)
 #define puf(a) printf("%llu\n",a)
 #define pii pair<int, int>
+#define pdd pair<double, double>
+#define mem(name, value) memset(name, value, sizeof(name))
+#define all(name) name.begin(),name.end()
 
 typedef long long ll;
 typedef unsigned long long ull;
 
-int dx[]={0,0,1,-1,-1,1,-1,1};
-int dy[]={-1,1,0,0,1,1,-1,-1};
-int dz[]={0,0,+1,-1,-1,+1,-1,+1};
+struct node
+{
+    double word;
+    bool end_mark;
+    node *next[257];
+    node()
+    {
+        end_mark = false;
+        word = 0;
+        for(int i=0;i<257;i++)
+            next[i] = NULL;
+    }
+}*root;
+
+void insert_word(char s[])
+{
+    node *c = root;
+    for(int i=0;s[i];i++)
+    {
+        int id = s[i];
+        if(c->next[id]==NULL)
+            c->next[id] = new node();
+        c = c->next[id];
+    }
+    c->end_mark = true;
+    c->word++;
+}
+
+double total;
+
+void traverse(node *r,char s[],int level=0)
+{
+    if(r->end_mark)
+    {
+        s[level]='\0';
+        printf("%s %.4lf\n",s,100.0*(r->word/total));
+        return;
+    }
+    for(int i=0;i<257;i++)
+        if(r->next[i])
+        {
+            s[level] = i;
+            traverse(r->next[i],s,level+1);
+        }
+}
+
+void del(node *r)
+{
+    for(int i=0;i<257;i++)
+        if(r->next[i])
+            del(r->next[i]);
+    delete(r);
+}
 
 int main()
 {
     //freopen("in.txt","r", stdin);
     //freopen("out.txt","w", stdout);
-
-    int t,kase=0;
-    double Size;
-    string s;
-    map<string,double>tree;
-    map<string,double>::iterator it;
-
-    scanf("%d",&t);
-    cin.ignore();
-    cin.ignore();
-
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    char spice[5000];
+    scanf("%d\n\n",&t);
     while(t--)
     {
-        kase++;
-
-        if(kase>1)
-            cout<<endl;
-
-        Size=0;
-
-        while(get_stl(s))
+        total = 0;
+        root = new node();
+        while(gets(spice))
         {
-            if(!s[0]||!cin)     break;
-            Size++;
-            tree[s]++;
+            if(!spice[0]) break;
+            total++;
+            insert_word(spice);
         }
-
-        for(it = tree.begin() ;it!=tree.end();it++)
-        {
-            double ans=(it->second/Size)*100.0;
-            printf("%s %0.4lf\n",it->first.c_str(),ans);
-        }
-        tree.clear();
+        traverse(root,spice);
+        del(root);
+        if(t)   puts("");
     }
-
     return 0;
 }
