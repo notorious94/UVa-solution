94f3acdd3ff14f2cebac28b44c6ec3bbbbf11e75
MD. Istiad Hossain <akib94dhk@gmail.com>
2018-07-24 20:48:55 +0600

    Create 10077 UVA.cpp

---
+++ 10077 UVA.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/// M A C R O Starts Here
+#define pf printf
+#define sf scanf
+#define MAX 500000
+#define MOD 100000007
+#define INF INT_MAX
+#define pi acos(-1.0)
+#define get_stl(s) getline(cin,s)
+#define sif(a) scanf("%d",&a)
+#define pif(a) printf("%d\n",a)
+#define puf(a) printf("%llu\n",a)
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define mem(name, value) memset(name, value, sizeof(name))
+#define all(name) name.begin(),name.end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int dx[]={0,0,1,-1,-1,1,-1,1};
+int dy[]={-1,1,0,0,1,1,-1,-1};
+int dz[]={0,0,1,-1,-1,1,-1,1};
+
+int isLeft(double a, double b, double c, double d, double e, double f)
+{
+    double r = 0.5*((a*(d-f))+(b*(e-c))+((c*f)-(d*e)));
+    if(r==0.00) return 0;
+    if(r<0)     return -1;
+    return 1;
+}
+
+double _distance(double x1,double y1,double x2,double y2)
+{
+    double x1x2 = x1 - x2;
+    double y1y2 = y1 - y2;
+    return sqrt((x1x2*x1x2)+(y1y2*y1y2));
+}
+
+int main()
+{
+    //freopen("in.txt","r", stdin);
+    //freopen("out.txt","w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int lu,ld,mu,md,ru,rd,au,ad;
+
+    while(scanf("%d%d",&au,&ad))
+    {
+        lu = rd = 0,ld = ru = 1;
+
+        if(au==1&&ad==1)    break;
+
+        double AU = au,AD = ad;
+
+        while(1)
+        {
+            mu = lu+ru;
+            md = ld+rd;
+
+            if(mu==au && md==ad)
+            {
+                puts("");
+                break;
+            }
+
+            double MU = mu;
+            double MD = md;
+
+            if((MU/MD)>(AU/AD))
+            {
+                ru = mu,rd = md;
+                printf("L");
+            }
+            else
+            {
+                lu = mu,ld = md;
+                printf("R");
+            }
+        }
+    }
+    return 0;
+}
