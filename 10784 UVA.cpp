4f1b68ef427e2f617e0bf00909fefa4e2d621c88
MD. Istiad Hossain <akib94dhk@gmail.com>
2018-07-24 10:32:58 +0600

    Create 10784 UVA.cpp

---
+++ 10784 UVA.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/// M A C R O Starts Here
+#define pf printf
+#define sf scanf
+#define MAX 500000
+#define MOD 100000007
+#define INF INT_MAX
+#define pi acos(-1.0)
+#define get_stl(s) getline(cin,s)
+#define sif(a) scanf("%d",&a)
+#define pif(a) printf("%d\n",a)
+#define puf(a) printf("%llu\n",a)
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define mem(name, value) memset(name, value, sizeof(name))
+#define all(name) name.begin(),name.end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int dx[]={0,0,1,-1,-1,1,-1,1};
+int dy[]={-1,1,0,0,1,1,-1,-1};
+int dz[]={0,0,1,-1,-1,1,-1,1};
+
+int isLeft(double a, double b, double c, double d, double e, double f)
+{
+    double r = 0.5*((a*(d-f))+(b*(e-c))+((c*f)-(d*e)));
+    if(r==0.00) return 0;
+    if(r<0)     return -1;
+    return 1;
+}
+
+double _distance(double x1,double y1,double x2,double y2)
+{
+    double x1x2 = x1 - x2;
+    double y1y2 = y1 - y2;
+    return sqrt((x1x2*x1x2)+(y1y2*y1y2));
+}
+
+int main()
+{
+    //freopen("in.txt","r", stdin);
+    //freopen("out.txt","w", stdout);
+    //ios_base::sync_with_stdio(false);
+    //cin.tie(NULL);
+
+    int t = 0,i;
+    ll N;
+    vector<ll>result;
+    vector<ll> :: iterator it;
+
+    for(i=4;;i++)
+    {
+        ll a = i;
+        if(a&1) a = i * ((a-3)/2);
+        else    a = (i/2) * (a-3);
+        if(a>1000000000000000)  break;
+        result.push_back(a);
+    }
+    while(scanf("%lld",&N)&&N)
+    {
+        it = lower_bound(all(result),N);
+        printf("Case %d: ",++t);
+        pif(4+it-result.begin());
+    }
+    //cout<<result[it - result.begin()]<<endl;
+    return 0;
+}
