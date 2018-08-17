6a54db05358360423f88ac3fc2bf7a6d6499dfaa
MD. Istiad Hossain <akib94dhk@gmail.com>
2018-08-17 12:18:22 +0600

    Create 11491 UVA.cpp

---
+++ 11491 UVA.cpp
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/// M A C R O Starts Here
+#define pf printf
+#define sf scanf
+#define MAX 500000
+#define MOD 100000007
+#define INF INT_MAX
+#define pi acos(-1.0)
+#define get_stl(s) getline(cin,s)
+#define sif(a) scanf("%d",&a)
+#define pif(a) printf("%d\n",a)
+#define puf(a) printf("%llu\n",a)
+#define pii pair<int, int>
+#define pdd pair<double, double>
+#define mem(name, value) memset(name, value, sizeof(name))
+#define all(name) name.begin(),name.end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int dx[]= {0,0,1,-1,-1,1,-1,1};
+int dy[]= {-1,1,0,0,1,1,-1,-1};
+int dz[]= {0,0,1,-1,-1,1,-1,1};
+
+int isLeft(double a, double b, double c, double d, double e, double f)
+{
+    double r = 0.5*((a*(d-f))+(b*(e-c))+((c*f)-(d*e)));
+    if(r==0.00) return 0;
+    if(r<0)     return -1;
+    return 1;
+}
+
+double _distance(double x1,double y1,double x2,double y2)
+{
+    double x1x2 = x1 - x2;
+    double y1y2 = y1 - y2;
+    return sqrt((x1x2*x1x2)+(y1y2*y1y2));
+}
+
+/*
+int lp[32001];
+vector<int>prime;
+
+void sieve(int limit = 32001)
+{
+    for(int i=2;i<limit;i++)
+    {
+        if(!lp[i])
+        {
+            lp[i] = i;
+            prime.push_back(i);
+        }
+        for(int j=0;j<prime.size()&&prime[j]<=i&&i*prime[j]<limit;j++)
+            lp[i*prime[j]]=i;
+    }
+}
+*/
+
+int main()
+{
+    //freopen("in.txt","r", stdin);
+    //freopen("out.txt","w", stdout);
+    string clone,s = "112233",number;
+
+    int n,d;
+    int freq[10];
+
+    while(scanf("%d%d",&n,&d)&&n&&d)
+    {
+        cin>>number;
+        int c = d;
+        for(int i=0;i!=number.size()-1;)
+        {
+            if(number[i]<number[i+1])
+            {
+                number.erase(number.begin()+i);
+                if(--d==0)  break;
+                i = max(i-1,0);
+            }
+            else    i++;
+        }
+        for(int i=0;i<n-c;i++)
+            cout<<number[i];
+        printf("\n");
+    }
+    return 0;
+}
