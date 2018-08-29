ed62fc540a7631046853fdb0a4eec2978619c2ec
MD. Istiad Hossain <akib94dhk@gmail.com>
2018-08-29 19:40:54 +0600

    Create 1210 UVA.cpp

---
+++ 1210 UVA.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/// M A C R O Starts Here
+#define pf printf
+#define sf scanf
+#define MAX 500000
+#define MOD 100000007
+#define INF INT_MAX
+#define LINF LONG_LONG_MAX
+#define pi acos(-1.0)
+#define get_stl(s) getline(cin,s)
+#define sif(a) scanf("%d",&a)
+#define pif(a) printf("%d\n",a)
+#define puf(a) printf("%llu\n",a)
+#define pii pair<int, int>
+#define p_b push_back
+#define debug cout<<"Reached Here"<<endl;
+#define pdd pair<double, double>
+#define mem(name, value) memset(name, value, sizeof(name))
+#define all(name) name.begin(),name.end()
+#define pq_min priority_queue< int, vector<int>, greater<int> >
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int dx[]={0,0,1,-1,-1,1,-1,1};
+int dy[]={-1,1,0,0,1,1,-1,-1};
+int dz[]={0,0,1,-1,-1,1,-1,1};
+
+int isLeft(double a, double b, double c, double d, double e, double f)
+{
+    double r = 0.5*((a*(d-f))+(b*(e-c))+((c*f)-(d*e)));
+    if(r==0.00) return 0;
+    if(r<0)     return -1;
+    return 1;
+}
+
+double _distance(double x1,double y1,double x2,double y2)
+{
+    double x1x2 = x1 - x2;
+    double y1y2 = y1 - y2;
+    return sqrt((x1x2*x1x2)+(y1y2*y1y2));
+}
+
+
+int lp[10000];
+vector<int>prime;
+
+void sieve(int limit = 10000)
+{
+    for(int i=2;i<limit;i++)
+    {
+        if(!lp[i])
+        {
+            lp[i] = i;
+            prime.push_back(i);
+        }
+        for(int j=0;j<prime.size()&&prime[j]<=i&&i*prime[j]<limit;j++)
+            lp[i*prime[j]]=i;
+    }
+}
+
+int DP[10001];
+
+int main()
+{
+    //freopen("in.txt","r", stdin);
+    //freopen("out.txt","w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    sieve();
+
+    for(int i=0;i<prime.size();i++)
+    {
+        int sum = prime[i];
+        for(int j=i+1;j<prime.size()&&sum<10001;j++)
+        {
+            DP[sum]++;
+            sum+=prime[j];
+        }
+    }
+    DP[9973] = 1;
+    int n;
+
+    while(scanf("%d",&n)&&n)
+        printf("%d\n",DP[n]);
+
+    return 0;
+}
