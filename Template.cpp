fa3413c6d6c2124a1116d842886f5362d4ca0b05
MD. Istiad Hossain <akib94dhk@gmail.com>
2018-08-29 19:41:17 +0600

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -7,21 +7,25 @@ using namespace std;
 #define MAX 500000
 #define MOD 100000007
 #define INF INT_MAX
+#define LINF LONG_LONG_MAX
 #define pi acos(-1.0)
 #define get_stl(s) getline(cin,s)
 #define sif(a) scanf("%d",&a)
 #define pif(a) printf("%d\n",a)
 #define puf(a) printf("%llu\n",a)
 #define pii pair<int, int>
+#define p_b push_back
+#define debug cout<<"Reached Here"<<endl;
 #define pdd pair<double, double>
 #define mem(name, value) memset(name, value, sizeof(name))
 #define all(name) name.begin(),name.end()
+#define pq_min priority_queue< int, vector<int>, greater<int> >
 
 typedef long long ll;
 typedef unsigned long long ull;
 
-int dx[]={-1,1,0,0,-1,1,-1,1};
-int dy[]={0,0,-1,1,1,-1,-1,1};
+int dx[]={0,0,1,-1,-1,1,-1,1};
+int dy[]={-1,1,0,0,1,1,-1,-1};
 int dz[]={0,0,1,-1,-1,1,-1,1};
 
 int isLeft(double a, double b, double c, double d, double e, double f)
@@ -39,12 +43,31 @@ double _distance(double x1,double y1,double x2,double y2)
     return sqrt((x1x2*x1x2)+(y1y2*y1y2));
 }
 
+/*
+int lp[32001];
+vector<int>prime;
+
+void sieve(int limit = 32001)
+{
+    for(int i=2;i<limit;i++)
+    {
+        if(!lp[i])
+        {
+            lp[i] = i;
+            prime.push_back(i);
+        }
+        for(int j=0;j<prime.size()&&prime[j]<=i&&i*prime[j]<limit;j++)
+            lp[i*prime[j]]=i;
+    }
+}
+*/
+
 int main()
 {
     //freopen("in.txt","r", stdin);
     //freopen("out.txt","w", stdout);
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+
     return 0;
 }
