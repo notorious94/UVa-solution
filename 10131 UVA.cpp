9b29deabfd136f0934ac003e465a43306fe49ac9
MD. Istiad Hossain <akib94dhk@gmail.com>
2018-09-10 01:00:44 +0600

    Create 10131 UVA.cpp

---
+++ 10131 UVA.cpp
@@ -0,0 +1,142 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/// M A C R O Starts Here
+#define pf printf
+#define sf scanf
+#define sif(n) scanf("%d",&n)
+#define lif(n) scanf("%lld",&n)
+#define MAX 1000005
+#define MOD 100000007
+#define INF INT_MAX
+#define LINF LONG_LONG_MAX
+#define pi acos(-1.0)
+#define get_stl(s) getline(cin,s)
+#define sif(a) scanf("%d",&a)
+#define pif(a) printf("%d\n",a)
+#define puf(a) printf("%llu\n",a)
+#define pii pair<int, int>
+#define p_b push_back
+#define ff first
+#define ss second
+#define debug cout<<"Reached Here"<<endl;
+#define pdd pair<double, double>
+#define mem(name, value) memset(name, value, sizeof(name))
+#define all(name) name.begin(),name.end()
+#define pq_min priority_queue< int, vector<int>, greater<int> >
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+enum COLOR { W = 0, G = 1, B = 2};
+
+int dx[]={0,0,1,-1,-1,1,-1,1};
+int dy[]={-1,1,0,0,1,1,-1,-1};
+int dz[]={0,0,1,-1,-1,1,-1,1};
+
+int isLeft(double a, double b, double c, double d, double e, double f)
+{
+    double r = 0.5*((a*(d-f))+(b*(e-c))+((c*f)-(d*e)));
+    if(r==0.00) return 0;
+    if(r<0)     return -1;
+    return 1;
+}
+
+double _distance(double x1,double y1,double x2,double y2)
+{
+    double x1x2 = x1 - x2;
+    double y1y2 = y1 - y2;
+    return sqrt((x1x2*x1x2)+(y1y2*y1y2));
+}
+
+/*
+int lp[32001];
+vector<int>prime;
+
+void sieve(int limit = 32001)
+{
+    for(int i=2;i<limit;i++)
+    {
+        if(!lp[i])
+        {
+            lp[i] = i;
+            prime.push_back(i);
+        }
+        for(int j=0;j<prime.size()&&prime[j]<=i&&i*prime[j]<limit;j++)
+            lp[i*prime[j]]=i;
+    }
+}
+*/
+class E
+{
+public:
+    int id,w,iq;
+    E(int a,int b,int c)
+    {
+        id = a,w = b,iq = c;
+    }
+    bool operator < (const E &p)
+    {
+        if(w!=p.w)  return w < p.w;
+        return iq > p.iq;
+    }
+};
+
+int main()
+{
+    //freopen("in.txt","r", stdin);
+    //freopen("out.txt","w", stdout);
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    vector<E>elephant;
+    vector<int>dp,track;
+
+    int w,iq,n=0,LIS = 1,start = 0;
+
+    while(scanf("%d%d",&w,&iq)==2)
+    {
+        elephant.push_back(E(++n,w,iq));
+        dp.push_back(1);
+        track.push_back(n-1);
+    }
+
+    sort(all(elephant));
+
+    for(int i=0;i<elephant.size()-1;i++)
+    {
+        for(int j=i+1;j<elephant.size();j++)
+        {
+            if(elephant[i].w < elephant[j].w && elephant[i].iq > elephant[j].iq && dp[j] <= dp[i]+1)
+            {
+                dp[j] = dp[i]+1;
+                track[j] = i;
+                if(LIS <= dp[j])
+                {
+                    LIS = dp[j];
+                    start = j;
+                }
+            }
+        }
+    }
+
+    printf("%d\n",LIS);
+    deque<int>dq;
+
+    while(1)
+    {
+        dq.push_front(elephant[start].id);
+        start = track[start];
+        if(start == track[start])
+        {
+            dq.push_front(elephant[start].id);
+            break;
+        }
+    }
+
+    while(dq.size())
+        printf("%d\n",dq.front()),dq.pop_front();
+
+    return 0;
+}
