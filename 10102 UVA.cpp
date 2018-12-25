271d0279d2403ec704cc472714107ef031413409
MD. Istiad Hossain <akib94dhk@gmail.com>
2018-12-25 10:57:59 +0600

    Update 10102 UVA.cpp

--- 10102 UVA.cpp
+++ 10102 UVA.cpp
@@ -1,82 +1,42 @@
 #include<bits/stdc++.h>
 #define pii pair<int,int>
-
 using namespace std;
-
-int m;
-
-vector<string>grid;
-int level[100][100];
-bool visit[100][100];
-
-int dx[]={0,0,1,-1,-1,1,-1,1};
-int dy[]={-1,1,0,0,1,1,-1,-1};
-
-bool inBound(int x,int y)
-{
-    return(x>=0&&x<m&&y>=0&&y<m);
-}
-
-int BFS(int i,int j)
-{
-    memset(visit,0,sizeof(visit));
-    level[i][j] = 0;
-    visit[i][j] = true;
-    //int max_dist = INT_MIN;
-
-    queue<pair<int,int> >q;
-
-    q.push({i,j});
-
-    while(q.size())
-    {
-        pii u = q.front();
-        q.pop();
-        for(int i=0;i<4;i++)
-        {
-            int vx = u.first+dx[i];
-            int vy = u.second+dy[i];
-            if(inBound(vx,vy)&&visit[vx][vy]==false)
-            {
-                visit[vx][vy] = true;
-                level[vx][vy] = level[u.first][u.second]+1;
-
-                if(grid[vx][vy]=='3')
-                    return level[vx][vy];
-                q.push({vx,vy});
-            }
-        }
-    }
-}
 
 int main()
 {
     //freopen("in.txt","r",stdin);
     //freopen("out.txt","w",stdout);
-
-    char s[500];
+    int m;
+    char val;
     while(scanf("%d",&m)==1)
     {
-        grid.clear();
-        for(int i=0;i<m;i++)
-        {
-            scanf("%s",s);
-            grid.push_back(s);
-        }
-
-        int ans = INT_MIN;
-
+        vector<pii>ones,threes;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(grid[i][j]=='1')
-                {
-                    ans = max(ans,BFS(i,j));
-                }
+                cin>>val;
+                if(val=='1')
+                    ones.push_back({i,j});
+                else if(val=='3')
+                    threes.push_back({i,j});
             }
+        }
+
+        int ans = INT_MIN;
+        for(int i=0;i<ones.size();i++)
+        {
+            int dist = INT_MAX;
+            for(int j=0;j<threes.size();j++)
+            {
+                int d = abs(ones[i].first-threes[j].first)+abs(ones[i].second-threes[j].second);
+                dist = min(dist,d);
+            }
+            ans = max(ans,dist);
         }
         cout<<ans<<endl;
     }
     return 0;
 }
+
+
