3c532e6a32e9871cc865cf252ec902db2dcde783
MD. Istiad Hossain <akib94dhk@gmail.com>
2019-02-15 01:16:23 +0600

    Update 10074 UVA.cpp

--- 10074 UVA.cpp
+++ 10074 UVA.cpp
@@ -5,9 +5,10 @@
 using namespace std;
 typedef long long ll;
 
-int hist[105];
-int grid[105][105];
+int hist[100];
+int grid[100][100];
 
+/*
 int getMaxArea(int n)
 {
 	stack<int> s;
@@ -34,29 +35,69 @@ int getMaxArea(int n)
 	}
 	return max_area;
 }
-/*
-int findArea(int r,int c)
-{
-    int maxArea = 0;
-    memset(hist,0,sizeof(hist));
-    for(int i=0;i<r;i++)
-    {
-        for(int j=0;j<c;j++)
-            (grid[i][j]==0) ? hist[j]=0 : hist[j]++;
-        maxArea = max(maxArea,getMaxArea(c));
-    }
-    cout<<maxArea<<endl;
-}
 */
+int maxAreaInHistogram(int len)
+{
+    stack<int>s;
+    s.push(0);
+    int i,top,area,maxArea=-1;
+    for(i=1; i<=len; i++)
+    {
+        if(i==len)
+        {
+            while(s.size())
+            {
+                top = s.top();
+                s.pop();
+                if(s.size()==0)
+                    area = hist[top]*i;
+                else
+                {
+                    int stackTop = s.top();
+                    area = hist[top]*(i-1-stackTop);
+                }
+                maxArea = max(area,maxArea);
+            }
+        }
+        else
+        {
+            if(s.size()==0 || hist[i]>=hist[s.top()])
+                s.push(i);
+            else
+            {
+                while(s.size())
+                {
+                    if(hist[s.top()]<=hist[i])
+                    {
+                        s.push(i);
+                        break;
+                    }
+                    top = s.top();
+                    s.pop();
+                    if(s.size()==0)
+                        area = hist[top]*i;
+                    else
+                    {
+                        int stackTop = s.top();
+                        area = hist[top]*(i-1-stackTop);
+                    }
+                    maxArea = max(area,maxArea);
+                }
+            }
+        }
+    }
+    return maxArea;
+}
+
 int main()
 {
     //freopen("in.txt","r",stdin);
     //freopen("out.txt","w",stdout);
 
     int r,c;
-    while(cin>>r>>c)
+    while(scanf("%d%d",&r,&c))
     {
-        if(r+c==0)  break;
+        if(r==c&&c==0)  break;
         int maxArea = 0;
         memset(hist,0,sizeof(hist));
         for(int i=0;i<r;i++)
@@ -65,9 +106,9 @@ int main()
             {
                 scanf("%d",&grid[i][j]);
                 grid[i][j] = !grid[i][j];
-                (grid[i][j]==0) ? hist[j]=0 : hist[j]++;
+                (grid[i][j]==0) ? hist[j] = 0 : hist[j]++;
             }
-            maxArea = max(maxArea,getMaxArea(c));
+            maxArea = max(maxArea,maxAreaInHistogram(c));
         }
         printf("%d\n",maxArea);
     }
