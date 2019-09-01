25d63e5166145d70ddb6fd2beb27187773c03df6
MD. Istiad Hossain <akib94dhk@gmail.com>
2019-09-02 01:14:49 +0600

    Create 11045 UVA.cpp

---
+++ 11045 UVA.cpp
@@ -0,0 +1,217 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+/*
+#pragma warning(disable:4786)
+#pragma warning(disable:4996)
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+*/
+using namespace std;
+
+/// M A C R O Starts Here
+#define MAX 500000
+#define MOD 100000007
+#define MOD2 998244353
+#define INF INT_MAX
+#define LINF LLONG_MAX
+#define pi acos(-1.0)
+#define get_stl(s) getline(cin,s)
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pdd pair<double, double>
+#define mem(name, value) memset(name, value, sizeof(name))
+#define all(name) name.begin(),name.end()
+#define pq_min priority_queue< int, vector<int>, greater<int> >
+#define dbg(args...)    do { cerr << #args << ' ' ; print(args); } while(0); cerr << endl;
+#define F first
+#define S second
+#define uniq(v) v.resize(unique(all(v)) - v.begin())
+#define howMany(v,L,R) (upper_bound(all(v),R)-lower_bound(all(v),L))
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int dx[]= {0,0,1,-1,-1,1,-1,1};
+int dy[]= {-1,1,0,0,1,1,-1,-1};
+int dz[]= {0,0,1,-1,-1,1,-1,1};
+int kx[]= {2,2,-2,-2,1,-1,1,-1};
+int ky[]= {1,-1,1,-1,2,2,-2,-2};
+
+/// Template Starts here
+
+template <typename T>
+void print(const T& x)
+{
+    cerr<<x<<' ';
+}
+
+template <typename T1, typename... T2>
+void print(const T1& first, const T2&... rest)
+{
+    print(first);
+    print(rest ...) ;
+}
+/// Template Ends here
+
+/// Functions Starts here
+
+/// Bit-masking
+int reset(int N,int pos){return N= N & ~(1<<pos);}
+bool check(ll N,ll pos){return (bool)(N & ((ll)1<<pos));}
+
+bool isPal(string s)
+{
+    int half = s.size()/2;
+    for(int i=0,j=s.size()-1;i<half;i++,j--)
+        if(s[i]!=s[j])  return false;
+    return true;
+}
+
+double _distance(double x1,double y1,double x2,double y2)
+{
+    double x1x2 = x1 - x2;
+    double y1y2 = y1 - y2;
+    return sqrt((x1x2*x1x2)+(y1y2*y1y2));
+}
+
+bool inBound(int x,int y,int r,int c)
+{
+    return (x>=0&&x<r&&y>=0&&y<c);
+}
+
+ll mulmod(ll a, ll b, ll c)
+{
+    ll x = 0, y = a % c;
+    while (b > 0)
+    {
+        if (b&1) x = (x + y) % c;
+        y = (y << 1) % c;
+        b >>= 1;
+    }
+    return x % c;
+}
+
+/*
+int lp[32001];
+vector<int>prime;
+void sieve(int limit = 32001)
+{
+    for(int i=2;i<limit;i++)
+    {
+        if(!lp[i])
+        {
+            lp[i] = i;
+            prime.push_back(i);
+        }
+        for(int j=0;j<prime.size()&&prime[j]<=i&&i*prime[j]<limit;j++)
+            lp[i*prime[j]]=i;
+    }
+}
+*/
+
+/// Functions Ends here
+
+map<string,int>shirt_id;
+int g[105][101];
+int parent[105];
+bool visit[105];
+
+bool isPath(int s,int d)
+{
+    queue<int>q;
+    memset(visit,false,sizeof(visit));
+    memset(parent,-1,sizeof(parent));
+    q.push(s);
+    visit[s] = true;
+    while(q.size())
+    {
+        int u = q.front();
+        q.pop();
+        for(int v=s;v<=d;v++)
+        {
+            if(!visit[v] && g[u][v] > 0)
+            {
+                visit[v] = true;
+                parent[v] = u;
+                q.push(v);
+                if(v==d)    return true;
+            }
+        }
+    }
+    return false;
+}
+
+int fordFulkerson(int source,int sink)
+{
+    int maxFlow = 0;
+    while(isPath(source,sink))
+    {
+        int v = sink,u;
+        int min_res_cap = INT_MAX;
+        while(parent[v]!=-1)
+        {
+            u = parent[v];
+            min_res_cap = min(min_res_cap,g[u][v]);
+            v = parent[v];
+        }
+        v = sink;
+        while(parent[v]!=-1)
+        {
+            u = parent[v];
+            g[u][v] -= min_res_cap;
+            g[v][u] += min_res_cap;
+            v = parent[v];
+        }
+        maxFlow+=min_res_cap;
+    }
+    return maxFlow;
+}
+
+int main()
+{
+    /// Suck my dick! HACKER -_-
+    //freopen("in.txt","r", stdin);
+    //freopen("out.txt","w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL),cout.tie(NULL);
+
+    shirt_id["XXL"]=1;
+    shirt_id["XL"]=2;
+    shirt_id["L"]=3;
+    shirt_id["M"]=4;
+    shirt_id["S"]=5;
+    shirt_id["XS"]=6;
+
+    int test,n,m;
+    cin>>test;
+    int kase = 0;
+    while(test--)
+    {
+        kase++;
+
+        cin>>n>>m;
+        mem(g,0);
+        int super_sink = 7+m*3;
+        string s1,s2;
+        for(int i=1;i<=6;i++)   g[0][i] = n/6;
+        for(int i=1;i<=m;i++)
+        {
+            cin>>s1>>s2;
+
+            int u = 7+(i-1)*2;
+            int v = 8+(i-1)*2;
+            int x = shirt_id[s1];
+            int y = shirt_id[s2];
+            g[x][u] = g[u][7+m*2+i-1] = 1;
+            g[y][v] = g[v][7+m*2+i-1] = 1;
+            g[7+m*2+i-1][super_sink] = 1;
+        }
+        int ans = fordFulkerson(0,super_sink);
+        if(ans<m)
+            cout<<"NO"<<endl;
+        else
+            cout<<"YES"<<endl;
+    }
+
+    return 0;
+}
